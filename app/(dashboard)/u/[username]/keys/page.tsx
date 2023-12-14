d9e6ea62b74ddeeb1e447f2cc1fabe6f36dbc005
Nhat Nguyen <nhatnguyenktz119@gmail.com>
2023-12-14 11:42:51 +0700

    Key settings

---
+++ app/(dashboard)/u/[username]/keys/page.tsx
@@ -0,0 +1,34 @@
+import { notFound } from 'next/navigation'
+
+import { getSelf } from '@/lib/auth-service'
+import { getStreamByUserId } from '@/lib/stream-service'
+
+import { UrlCard } from './_components/url-card'
+import { KeyCard } from './_components/key-card'
+import { ConnectModal } from './_components/connect-modal'
+
+const KeysPage = async () => {
+  const self = await getSelf()
+  const stream = await getStreamByUserId(self.id)
+
+  if (!stream) {
+    return notFound()
+  }
+
+  return (
+    <div className="p-6">
+      <div className="mb-4 flex items-center justify-between">
+        <h1 className="text-2xl font-bold">Keys & URLs</h1>
+
+        <ConnectModal />
+      </div>
+
+      <div className="space-y-4">
+        <UrlCard value={stream.serverUrl} />
+        <KeyCard value={stream.streamKey} />
+      </div>
+    </div>
+  )
+}
+
+export default KeysPage
