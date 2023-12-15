714626174f64a935f2d4d1a0bff7e1d04a295f40
Nhat Nguyen <nhatnguyenktz119@gmail.com>
2023-12-15 18:58:22 +0700

    Home page

--- app/(browse)/(home)/page.tsx
+++ app/(browse)/(home)/page.tsx
@@ -1,5 +1,14 @@
+import { Suspense } from 'react'
+import { Results, ResultsSkeleton } from './_components/results'
+
 const HomePage = () => {
-  return <div className=""></div>
+  return (
+    <div className="mx-auto h-full max-w-screen-2xl p-8">
+      <Suspense fallback={<ResultsSkeleton />}>
+        <Results />
+      </Suspense>
+    </div>
+  )
 }
 
 export default HomePage
---
+++ components/thumbnail.tsx
@@ -0,0 +1,47 @@
+import Image from 'next/image'
+
+import { UserAvatar } from './user-avatar'
+import { Skeleton } from './ui/skeleton'
+
+interface ThumbnailProps {
+  src: string | null
+  fallback: string
+  username: string
+  isLive: boolean
+}
+
+export const Thumbnail = ({ fallback, src, username, isLive }: ThumbnailProps) => {
+  let content
+
+  if (!src) {
+    content = (
+      <div className="flex h-full w-full flex-col items-center justify-center gap-y-4 rounded-md bg-background transition-transform group-hover:-translate-y-1 group-hover:translate-x-2">
+        <UserAvatar size={'lg'} showBadge username={username} imageUrl={fallback} isLive={isLive} />
+      </div>
+    )
+  } else {
+    content = (
+      <Image
+        src={src}
+        alt={`thumbnail - ${username}`}
+        fill
+        className="rounded-md object-cover transition-transform group-hover:-translate-y-2 group-hover:translate-x-2"
+      />
+    )
+  }
+
+  return (
+    <div className="group relative aspect-video cursor-pointer rounded-md">
+      <div className="absolute inset-0 flex items-center justify-center rounded-md bg-blue-600 opacity-0 group-hover:opacity-100" />
+      {content}
+    </div>
+  )
+}
+
+export const ThumbnailSkeleton = () => {
+  return (
+    <div className="group relative aspect-video cursor-pointer rounded-xl">
+      <Skeleton className="h-full w-full" />
+    </div>
+  )
+}
